b19d7a383b6ed26f4e4cd713d06aedd0bef2c803
CyberSage5 <terfajohn45@gmail.com>
2024-06-20 09:39:04 +0100

    fix

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -10,6 +10,7 @@ import {
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 import { updateInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
 
 export default function EditInvoiceForm({
   invoice,
@@ -129,4 +130,3 @@ export default function EditInvoiceForm({
     </form>
     </form>
   );
-}
